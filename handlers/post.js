dbe9c794dd4968045d796c970410bdc2584a5a62
Marcus Ellis <mellis@atlassian.com>
2018-12-07 16:32:46 -0600

    refactor part 3

--- handlers/post.js
+++ handlers/post.js
@@ -1,6 +1,17 @@
 const slack = require('../lib/slack');
 const moment = require('moment');
 
+// the end event can come some time after the actual end, and in the case of
+// back to back meeetings, we might actually receive the end event for the
+// first meeting after the start event of the second meeting. so we use this
+// to keep track whether the end event is for the same start event.
+// and yes, this should be stored in a more persistent way, but i'm lazy.
+let prevUrl = '';
+
+const DND_TOKEN = ' [DND]';
+const DATE_FORMAT = 'MMM D, YYYY [at] hh:mmA';
+const EMOJI = ':calendar:';
+
 module.exports = (req, res, next) => {
   // check for secret token
   if (!req.body.token || req.body.token !== process.env.SECRET_TOKEN) {
@@ -9,9 +20,12 @@ module.exports = (req, res, next) => {
     return;
   }
 
-  let clearStatus = req.body.clear || false;
+  let status = req.body.title;
+  const url = req.body.url;
+  const clearStatus = req.body.clear && url === prevUrl;
 
   if (clearStatus) {
+    prevUrl = '';
     slack.status({})
     .then(() => {
       res.status(200);
@@ -24,14 +38,11 @@ module.exports = (req, res, next) => {
     return;
   }
 
-  // grab status and clean it up
-  let status = req.body.title;
-  const dndToken = ' [DND]';
+  prevUrl = url;
 
   // parse event start/stop time
-  const dateFormat = 'MMM D, YYYY [at] hh:mmA';
-  const start = moment(req.body.start, dateFormat);
-  const end = moment(req.body.end, dateFormat);
+  const start = moment(req.body.start, DATE_FORMAT);
+  const end = moment(req.body.end, DATE_FORMAT);
   const duration = end.diff(start, 'minutes');
 
   if (!status) {
@@ -41,15 +52,15 @@ module.exports = (req, res, next) => {
   }
 
   // check for DND
-  if (status.includes(dndToken)) {
+  if (status.includes(DND_TOKEN)) {
     slack.dnd(duration);
-    status = status.replace(dndToken, '');
+    status = status.replace(DND_TOKEN, '');
   }
 
   // set status
   slack.status({
     text: `${status} from ${start.format('h:mm')} to ${end.format('h:mm a')} ${process.env.TIME_ZONE}`,
-    emoji: ":calendar:"
+    emoji: EMOJI
   })
   .then(() => {
     res.status(200);
